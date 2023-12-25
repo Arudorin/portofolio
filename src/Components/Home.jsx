a57fb9c1ae5e286bd74868751dfd948a11b71824
Arudorin <ekakusmayani91@gmail.com>
2023-12-25 16:12:17 +0800

    Update launch.json and About.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "../images/matese-fields-pvHma684eEI-unsplash.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "man starring at white sky";
 
 const Home = ({ name, title }) => {
   return (
